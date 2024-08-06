51f87d6dde930a0444ba0b998679b5d1a8ed914d
dinopegassi <nmbost@gmail.com>
2024-08-07 06:51:09 +0930

    avanzado en entendimiento de dynamic vs static pages

--- EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/meadowlark.js
+++ EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/meadowlark.js
@@ -1,2 +1,41 @@
 const express = require("express");
+const expresshandlebars = require("express-handlebars");
+const port = process.env.PORT || 3000;
+
 const app = express();
+
+app.engine(
+  "handlebars",
+  expresshandlebars.engine({
+    defaultLayout: "coso",
+  })
+);
+app.set("view engine", "handlebars");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.type("text/plain");
+  res.send("chupala yastin");
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+
+app.use((req, res) => {
+  res.type("text/plain");
+  res.status(404);
+  res.send("404 - Not he encontrado");
+});
+
+app.use((err, req, res, next) => {
+  res.type("text/plain");
+  res.status(500);
+  res.send("500 - Server error");
+});
+
+app.listen(port, () => {
+  console.log(
+    `esto ta andando perri en el puerto ${port}\n` + `cortalo con coca o ctrl-c`
+  );
+});
