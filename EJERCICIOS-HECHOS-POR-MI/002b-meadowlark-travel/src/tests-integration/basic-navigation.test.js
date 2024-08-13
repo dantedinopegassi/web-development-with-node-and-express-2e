01d23cf5588def0bfb2ad1bc5db6bebd731cb3e9
dinopegassi <nmbost@gmail.com>
2024-08-13 10:11:47 +0930

    integration test works

--- EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/tests-integration/basic-navigation.test.js
+++ EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/tests-integration/basic-navigation.test.js
@@ -1,65 +1,33 @@
 const puppeteer = require("puppeteer");
 const app = require("../meadowlark.js");
 
+let server;
+let port = 3000;
 
-
-let server = null;
-let port = null;                                //ESTO AUN NO ANDA: tira este error
-                                                // PASS src/modules/__tests__/handlers.test.js
-                                                // FAIL src/tests-integration/basic-navigation.test.js
-                                                //   ● about to home page
-
-                                                //     No element found for selector: [id-identificador-test-puppeteer='home']
-
-                                                //       28 |     const page = await browser.newPage();
-                                                //       29 |     await page.goto(`http://localhost:${port}/`);
-                                                //     > 30 |     await Promise.all([
-                                                //          |     ^
-                                                //       31 |         page.waitForNavigation(),
-                                                //       32 |         page.click("[id-identificador-test-puppeteer='home']")
-                                                //       33 |     ]);
-
-                                                //       at assert (node_modules/puppeteer-core/src/util/assert.ts:19:11)
-                                                //       at CdpFrame.click (node_modules/puppeteer-core/src/api/Frame.ts:1051:11)
-                                                //           at async Promise.all (index 1)
-                                                //       at Object.<anonymous> (src/tests-integration/basic-navigation.test.js:30:5)
-
-                                                // A worker process has failed to exit gracefully and has been force exited. This is likely caused by tests leaking due to improper teardown. Try running with --detectOpenHandles to find leaks. Active timers can also cause this, ensure that .unref() was called on them.
-                                                // Test Suites: 1 failed, 1 passed, 2 total
-                                                // Tests:       1 failed, 4 passed, 5 total
-                                                // Snapshots:   0 total
-                                                // Time:        4.169 s
-                                                // Ran all test suites.
-
-beforeEach(async() => {
-    port = 3000;
-    server = app.listen(port);
+beforeEach(async () => {
+    server = app.listen(port, () => {
+        console.log("el server arranco joya");
+    });
 });
 
 afterEach(() => {
     server.close();
 });
 
-test('about to home page', async() => {
+test("about to home page", async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    // await page.goto(`http://localhost:${port}`);
-    await page.goto(`https://news.yahoo.com/`);
+    await page.goto(`http://localhost:${port}/about`);
 
-    const headings = await page.$$('h3');
-    for (let index = 0; index < headings.length; index++) {
-        const element = headings[index];
-        const heading = await page.evaluate(thing => thing.textContent, thing);
-        console.log(heading);
-    }
+    await page.waitForSelector('[id-identificador-test-puppeteer="home"]', {
+        timeout: 5000,
+    });
 
-    // const elementos = await page.$$('div');
-    // console.log(elementos.toString());
-    // await Promise.all([
-    //     page.waitForNavigation(),
-    //     page.click('[id-identificador-test-puppeteer="home"]')
-    // ]);
-    // expect(page.url()).toBe(`http://localhost:${port}`);
+    await Promise.all([
+        page.waitForNavigation({waitUntil: 'networkidle0'}),
+        page.click('[id-identificador-test-puppeteer="home"]')
+    ]);
+    expect(page.url()).toBe(`http://localhost:${port}/`);
 
     await browser.close();
-},15000)
+});
--- EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/tests-integration/coso.js
+++ EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/src/tests-integration/coso.js
@@ -1,25 +1,17 @@
-const puppeteer = require('puppeteer');
+// const puppeteer = require('puppeteer');
 
-(async() => {
-    const browser = await puppeteer.launch();
-    const page = await browser.newPage();
-    // await page.goto(`http://localhost:${port}`);
-    await page.goto(`https://news.yahoo.com/`);
+// (async() => {
+//     const browser = await puppeteer.launch();
+//     const page = await browser.newPage();
+//     // await page.goto(`http://localhost:${port}`);
+//     await page.goto(`https://news.yahoo.com/`);
 
-    const headings = await page.$$('h3');
-    for (let index = 0; index < headings.length; index++) {
-        const element = headings[index];
-        const heading = await page.evaluate(element => element.textContent, element);
-        console.log(heading);
-    }
+//     const headings = await page.$$('h3');
+//     for (let index = 0; index < headings.length; index++) {
+//         const element = headings[index];
+//         const heading = await page.evaluate(element => element.textContent, element);
+//         console.log(heading);
+//     }
 
-    // const elementos = await page.$$('div');
-    // console.log(elementos.toString());
-    // await Promise.all([
-    //     page.waitForNavigation(),
-    //     page.click('[id-identificador-test-puppeteer="home"]')
-    // ]);
-    // expect(page.url()).toBe(`http://localhost:${port}`);
-
-    await browser.close();
-})();
+//     await browser.close();
+// })();
