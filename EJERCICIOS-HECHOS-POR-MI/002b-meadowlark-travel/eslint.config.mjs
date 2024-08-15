33a3b23e5baf72cb360f9f513e674c98ca163d77
dinopegassi <nmbost@gmail.com>
2024-08-15 10:50:05 +0930

    ESLINT CONFIG 9.X.X TOMA JEST, VAAAAMOOOO

--- EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/eslint.config.mjs
+++ EJERCICIOS-HECHOS-POR-MI/002b-meadowlark-travel/eslint.config.mjs
@@ -1,9 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
+// nidea por que pinches eslint cambio el formato de configuracion que tenia...
+// antes era        env: {"jest": true}
+// ahora es asi:    ...globals.jest
+// vaya dio a saber por que es asi... +Info: 
+// https://eslint.org/docs/latest/use/configure/migration-guide#configuring-language-options
 
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
-  pluginJs.configs.recommended,
+    { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+    {
+        languageOptions: {
+            globals: {
+                ...globals.node,
+                ...globals.jest,
+            },
+        },
+    },
+    pluginJs.configs.recommended,
 ];
